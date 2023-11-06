690ee683f4aad9d244d368c238e5fe7d4bd801d4
AndreaGul <andreagulino99@gmail.com>
2023-11-06 23:56:30 +0100

    feat:prezzo biglietto prima degli eventuali sconti

--- js/main.js
+++ js/main.js
@@ -5,18 +5,27 @@
 // 1. Con un prompt chiediamo all'utente il numero di chilometri da percorrere, il valore comunicato dall'utente dovra essere trasformato in un numero con parseint e salveremo il valore in una varibile chiamata kmUtente.
 
 let kmUtente = parseInt(
-  prompt('Inserire il numeri di chilometri da percorere')
+  prompt('Inserire il numeri di chilometri da percorrere')
 );
+
+console.log('chilometri che il passeggero deve percorrere:', kmUtente);
 
 // 2. Con un'altro prompt chiediamo l'età del passeggero il valore comunicato dall'utente dovra essere trasformato in un numero con parseint e salveremo il valore il una variabile che chiameremo etaUtente.
 
 let etaUtente = parseInt(prompt("Inserire l'età del passeggero"));
 
+console.log('età del passeggero:', etaUtente);
+
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 
 // 3.  Il prezzo del biglietto è definito in base ai km (0.21 € al km),
-//     quindi dichiariamo una variabile prezzoBiglietto che avra come valore la moltiplicazione dei numeri ossia i chilometri che l'utente deve percorrere conservati nella variabile kmUtente e il prezzo del biglietto a kilometro che dichiariando una variabile prezzo e definendo il suo valore come 0.21.
+//     quindi dichiariamo una variabile prezzoBiglietto che avra come valore la moltiplicazione dei due numeri ossia i chilometri che l'utente deve percorrere conservati nella variabile kmUtente e il prezzo del biglietto a kilometro che dichiariando una variabile prezzo e definendo il suo valore come 0.21.
 
+const prezzo = 0.21;
+
+let prezzoBiglietto = kmUtente * prezzo;
+
+console.log('prezzo del biglietto senza sconti:', prezzoBiglietto);
 // 4.  Va applicato uno sconto del 20% per i minorenni,
 //     quindi se l'età dell'utente è minore di 18 il alla variabile prezzoBiglietto dovra essere sottratto il 20% del suo valore.
 //     il valore del 20% sarà definito in una varibile che chiamiamo scontoUnder18.
